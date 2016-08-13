f3caa7dfff6c5a17be3df0bb3e41b9d97e057865
Domingo Suarez Torres <domingo.suarez@gmail.com>
2016-08-13 11:37:50 -0500

    se agrega servicio de calculo

---
+++ src/main/java/com/cacti/workshop/microservices/service/CalculadoraService.java
@@ -0,0 +1,17 @@
+package com.cacti.workshop.microservices.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by domix on 8/13/16.
+ */
+@Service
+public class CalculadoraService {
+  public Integer sum(Integer ope1, Integer ope2) {
+    return ope1 + ope2;
+  }
+
+  public Integer subs(Integer ope1, Integer ope2) {
+    return ope2 - ope1;
+  }
+}
